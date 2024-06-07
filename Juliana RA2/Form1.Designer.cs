032b22b0ac688180d0cc67a416b40df3c1d9a485
Julianacivil03 <civiljuliana407@gmail.com>
2024-06-07 19:01:50 -0400

    Add files via upload

---
+++ Juliana RA2/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Juliana_RA2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSueldoNeto = new System.Windows.Forms.TextBox();
+            this.lblSueldoNeto = new System.Windows.Forms.Label();
+            this.txtHijos = new System.Windows.Forms.TextBox();
+            this.txtIncentivos = new System.Windows.Forms.TextBox();
+            this.txtSueldo = new System.Windows.Forms.TextBox();
+            this.lblIncentivos = new System.Windows.Forms.Label();
+            this.lblHijos = new System.Windows.Forms.Label();
+            this.lblSueldo = new System.Windows.Forms.Label();
+            this.txtDescuentoSFS = new System.Windows.Forms.TextBox();
+            this.txtTotalDescuentos = new System.Windows.Forms.TextBox();
+            this.txtDescuentoAFP = new System.Windows.Forms.TextBox();
+            this.txtSueldoBrutoIncentivos = new System.Windows.Forms.TextBox();
+            this.lblDescuentoSFS = new System.Windows.Forms.Label();
+            this.lblDescuentoAFP = new System.Windows.Forms.Label();
+            this.lblSueldoBrutoIncentivos = new System.Windows.Forms.Label();
+            this.lblTotalDescuentos = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSueldoNeto
+            // 
+            this.txtSueldoNeto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSueldoNeto.Location = new System.Drawing.Point(272, 149);
+            this.txtSueldoNeto.Name = "txtSueldoNeto";
+            this.txtSueldoNeto.Size = new System.Drawing.Size(111, 30);
+            this.txtSueldoNeto.TabIndex = 24;
+            // 
+            // lblSueldoNeto
+            // 
+            this.lblSueldoNeto.AutoSize = true;
+            this.lblSueldoNeto.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSueldoNeto.Location = new System.Drawing.Point(7, 203);
+            this.lblSueldoNeto.Name = "lblSueldoNeto";
+            this.lblSueldoNeto.Size = new System.Drawing.Size(130, 27);
+            this.lblSueldoNeto.TabIndex = 23;
+            this.lblSueldoNeto.Text = "Sueldo Neto";
+            // 
+            // txtHijos
+            // 
+            this.txtHijos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHijos.Location = new System.Drawing.Point(272, 89);
+            this.txtHijos.Name = "txtHijos";
+            this.txtHijos.Size = new System.Drawing.Size(111, 30);
+            this.txtHijos.TabIndex = 22;
+            // 
+            // txtIncentivos
+            // 
+            this.txtIncentivos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIncentivos.Location = new System.Drawing.Point(272, 204);
+            this.txtIncentivos.Name = "txtIncentivos";
+            this.txtIncentivos.Size = new System.Drawing.Size(111, 30);
+            this.txtIncentivos.TabIndex = 21;
+            // 
+            // txtSueldo
+            // 
+            this.txtSueldo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSueldo.Location = new System.Drawing.Point(272, 34);
+            this.txtSueldo.Name = "txtSueldo";
+            this.txtSueldo.Size = new System.Drawing.Size(111, 30);
+            this.txtSueldo.TabIndex = 20;
+            // 
+            // lblIncentivos
+            // 
+            this.lblIncentivos.AutoSize = true;
+            this.lblIncentivos.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIncentivos.Location = new System.Drawing.Point(7, 133);
+            this.lblIncentivos.Name = "lblIncentivos";
+            this.lblIncentivos.Size = new System.Drawing.Size(112, 27);
+            this.lblIncentivos.TabIndex = 19;
+            this.lblIncentivos.Text = "Incentivos";
+            // 
+            // lblHijos
+            // 
+            this.lblHijos.AutoSize = true;
+            this.lblHijos.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHijos.Location = new System.Drawing.Point(7, 87);
+            this.lblHijos.Name = "lblHijos";
+            this.lblHijos.Size = new System.Drawing.Size(170, 27);
+            this.lblHijos.TabIndex = 18;
+            this.lblHijos.Text = "Número de hijos";
+            // 
+            // lblSueldo
+            // 
+            this.lblSueldo.AutoSize = true;
+            this.lblSueldo.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSueldo.Location = new System.Drawing.Point(7, 34);
+            this.lblSueldo.Name = "lblSueldo";
+            this.lblSueldo.Size = new System.Drawing.Size(77, 27);
+            this.lblSueldo.TabIndex = 17;
+            this.lblSueldo.Text = "Sueldo";
+            // 
+            // txtDescuentoSFS
+            // 
+            this.txtDescuentoSFS.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescuentoSFS.Location = new System.Drawing.Point(272, 360);
+            this.txtDescuentoSFS.Name = "txtDescuentoSFS";
+            this.txtDescuentoSFS.Size = new System.Drawing.Size(111, 30);
+            this.txtDescuentoSFS.TabIndex = 32;
+            // 
+            // txtTotalDescuentos
+            // 
+            this.txtTotalDescuentos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotalDescuentos.Location = new System.Drawing.Point(272, 420);
+            this.txtTotalDescuentos.Name = "txtTotalDescuentos";
+            this.txtTotalDescuentos.Size = new System.Drawing.Size(111, 30);
+            this.txtTotalDescuentos.TabIndex = 31;
+            // 
+            // txtDescuentoAFP
+            // 
+            this.txtDescuentoAFP.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescuentoAFP.Location = new System.Drawing.Point(272, 310);
+            this.txtDescuentoAFP.Name = "txtDescuentoAFP";
+            this.txtDescuentoAFP.Size = new System.Drawing.Size(111, 30);
+            this.txtDescuentoAFP.TabIndex = 30;
+            // 
+            // txtSueldoBrutoIncentivos
+            // 
+            this.txtSueldoBrutoIncentivos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSueldoBrutoIncentivos.Location = new System.Drawing.Point(272, 261);
+            this.txtSueldoBrutoIncentivos.Name = "txtSueldoBrutoIncentivos";
+            this.txtSueldoBrutoIncentivos.Size = new System.Drawing.Size(111, 30);
+            this.txtSueldoBrutoIncentivos.TabIndex = 29;
+            // 
+            // lblDescuentoSFS
+            // 
+            this.lblDescuentoSFS.AutoSize = true;
+            this.lblDescuentoSFS.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescuentoSFS.Location = new System.Drawing.Point(7, 360);
+            this.lblDescuentoSFS.Name = "lblDescuentoSFS";
+            this.lblDescuentoSFS.Size = new System.Drawing.Size(151, 27);
+            this.lblDescuentoSFS.TabIndex = 28;
+            this.lblDescuentoSFS.Text = "DescuentoSFS";
+            // 
+            // lblDescuentoAFP
+            // 
+            this.lblDescuentoAFP.AutoSize = true;
+            this.lblDescuentoAFP.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescuentoAFP.Location = new System.Drawing.Point(7, 310);
+            this.lblDescuentoAFP.Name = "lblDescuentoAFP";
+            this.lblDescuentoAFP.Size = new System.Drawing.Size(158, 27);
+            this.lblDescuentoAFP.TabIndex = 27;
+            this.lblDescuentoAFP.Text = "DescuentoAFP";
+            // 
+            // lblSueldoBrutoIncentivos
+            // 
+            this.lblSueldoBrutoIncentivos.AutoSize = true;
+            this.lblSueldoBrutoIncentivos.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSueldoBrutoIncentivos.Location = new System.Drawing.Point(-5, 261);
+            this.lblSueldoBrutoIncentivos.Name = "lblSueldoBrutoIncentivos";
+            this.lblSueldoBrutoIncentivos.Size = new System.Drawing.Size(268, 27);
+            this.lblSueldoBrutoIncentivos.TabIndex = 26;
+            this.lblSueldoBrutoIncentivos.Text = "Sueldo Bruto y  Incentivos";
+            // 
+            // lblTotalDescuentos
+            // 
+            this.lblTotalDescuentos.AutoSize = true;
+            this.lblTotalDescuentos.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalDescuentos.Location = new System.Drawing.Point(7, 420);
+            this.lblTotalDescuentos.Name = "lblTotalDescuentos";
+            this.lblTotalDescuentos.Size = new System.Drawing.Size(177, 27);
+            this.lblTotalDescuentos.TabIndex = 25;
+            this.lblTotalDescuentos.Text = "Total Descuentos";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(170, 503);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(152, 67);
+            this.btnCalcular.TabIndex = 33;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.ClientSize = new System.Drawing.Size(538, 590);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtDescuentoSFS);
+            this.Controls.Add(this.txtTotalDescuentos);
+            this.Controls.Add(this.txtDescuentoAFP);
+            this.Controls.Add(this.txtSueldoBrutoIncentivos);
+            this.Controls.Add(this.lblDescuentoSFS);
+            this.Controls.Add(this.lblDescuentoAFP);
+            this.Controls.Add(this.lblSueldoBrutoIncentivos);
+            this.Controls.Add(this.lblTotalDescuentos);
+            this.Controls.Add(this.txtSueldoNeto);
+            this.Controls.Add(this.lblSueldoNeto);
+            this.Controls.Add(this.txtHijos);
+            this.Controls.Add(this.txtIncentivos);
+            this.Controls.Add(this.txtSueldo);
+            this.Controls.Add(this.lblIncentivos);
+            this.Controls.Add(this.lblHijos);
+            this.Controls.Add(this.lblSueldo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSueldoNeto;
+        private System.Windows.Forms.Label lblSueldoNeto;
+        private System.Windows.Forms.TextBox txtHijos;
+        private System.Windows.Forms.TextBox txtIncentivos;
+        private System.Windows.Forms.TextBox txtSueldo;
+        private System.Windows.Forms.Label lblIncentivos;
+        private System.Windows.Forms.Label lblHijos;
+        private System.Windows.Forms.Label lblSueldo;
+        private System.Windows.Forms.TextBox txtDescuentoSFS;
+        private System.Windows.Forms.TextBox txtTotalDescuentos;
+        private System.Windows.Forms.TextBox txtDescuentoAFP;
+        private System.Windows.Forms.TextBox txtSueldoBrutoIncentivos;
+        private System.Windows.Forms.Label lblDescuentoSFS;
+        private System.Windows.Forms.Label lblDescuentoAFP;
+        private System.Windows.Forms.Label lblSueldoBrutoIncentivos;
+        private System.Windows.Forms.Label lblTotalDescuentos;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Juliana RA2/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Juliana_RA2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Leer el sueldo del empleado
+                decimal sueldoBruto = decimal.Parse(txtSueldo.Text);
+
+                // Leer el número de hijos
+                int numeroHijos = int.Parse(txtHijos.Text);
+
+                // Calcular incentivos
+                decimal incentivos = numeroHijos * 500;
+
+                // Calcular sueldo bruto más incentivos
+                decimal sueldoConIncentivos = sueldoBruto + incentivos;
+
+                // Calcular descuentos
+                decimal descuentoAFP = sueldoConIncentivos * 0.0287m;
+                decimal descuentoSFS = sueldoConIncentivos * 0.0304m;
+                decimal totalDescuentos = descuentoAFP + descuentoSFS;
+
+                // Calcular sueldo neto
+                decimal sueldoNeto = sueldoConIncentivos - totalDescuentos;
+
+                // Mostrar los resultados
+                txtIncentivos.Text = incentivos.ToString("C");
+                txtSueldoBrutoIncentivos.Text = sueldoConIncentivos.ToString("C");
+                txtDescuentoAFP.Text = descuentoAFP.ToString("C");
+                txtDescuentoSFS.Text = descuentoSFS.ToString("C");
+                txtTotalDescuentos.Text = totalDescuentos.ToString("C");
+                txtSueldoNeto.Text = sueldoNeto.ToString("C");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+    }
+
